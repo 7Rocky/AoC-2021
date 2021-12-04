d66b75a174ad1dd81bffc9f812d56afa32407a81
7Rocky <rockygitt@gmail.com>
2021-12-04 12:45:36 +0100

    Tests

---
+++ day_04/main_test.py
@@ -0,0 +1,23 @@
+import io
+import sys
+import unittest
+
+from main import main
+
+
+class TestMain(unittest.TestCase):
+    def test_main(self):
+        rescued_stdout = io.StringIO()
+        sys.stdout = rescued_stdout
+
+        main()
+
+        want = 'First board to win (1): 8580\n' + \
+               'Last board to win (2): 9576\n'
+
+        sys.stdout = sys.__stdout__
+        self.assertEqual(want, rescued_stdout.getvalue())
+
+
+if __name__ == '__main__':
+    unittest.main()
